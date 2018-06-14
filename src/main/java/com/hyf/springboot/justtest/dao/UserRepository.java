a8c4f91c37aada7b2afff5928ba56602ce14689b
hyf <hyf.an@qq.com>
2018-06-14 20:36:28 +0800

    修改代码格式

--- src/main/java/com/hyf/springboot/justtest/dao/UserRepository.java
+++ src/main/java/com/hyf/springboot/justtest/dao/UserRepository.java
@@ -6,10 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import javax.transaction.Transactional;
 import java.util.List;
 
-public interface UserRepository extends JpaRepository<User,Integer> {
-    User findUserByUsernameAndDeleted(String username,boolean deleted);
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findUserByUsernameAndDeleted(String username, boolean deleted);
 
     List<User> findUserByDeleted(boolean deleted);
+
     @Transactional
     void deleteUserByUsername(String username);
 
